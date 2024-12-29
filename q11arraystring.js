c68bfc697f9928fe3f700a896ef5c9a8976dc03e
Rupayan Das <hrupayandas@gmail.com>
2024-12-29 14:34:49 +0530

    q11 assignment

---
+++ q11arraystring.js
@@ -0,0 +1,11 @@
+var temperature = [31,32,33,34,35,36,37];
+
+console.log(temperature);
+
+var fourthDay = temperature[3];
+
+console.log("Temperature of 4th day "+fourthDay);
+
+temperature[3] = 36;
+
+console.log(temperature);

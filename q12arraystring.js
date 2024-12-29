0980e1139cad7fde1b56008bae3a1a4e2e2df1a4
Rupayan Das <hrupayandas@gmail.com>
2024-12-29 14:40:03 +0530

    q12 assignment

---
+++ q12arraystring.js
@@ -0,0 +1,5 @@
+var toDoList = ["Make Breakfast", "Bath", "Attend Class", "Do the assignment", "Yoga"];
+toDoList.unshift("Check Mails");
+// console.log(toDoList);
+toDoList.pop();
+console.log(toDoList);
